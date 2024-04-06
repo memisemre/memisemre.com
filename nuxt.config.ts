41189c3ec43819a2319522c0be5c8abd3e72e837
Emre Memiş <dev.emrememis@gmail.com>
2024-04-06 16:37:01 +0300

    install tailwind and create scss architecht

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite:{
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/_colors.scss" as *;'
+        }
+      }
+    }
+  },
+  css: ['~/assets/css/tailwind.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
-    <h1>Hello World. Memisemre.com</h1>
+    <h1 class="text-3xl font-bold">Hello World. Memisemre.com</h1>
 </template>
 <script setup>
 definePageMeta({
     layout: "default"
 })
 </script>
+
+<style lang="scss">
+
+h1{
+    color: $primary-color;
+}
+</style>
